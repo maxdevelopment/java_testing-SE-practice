b5886febf9155e1396f230a3ce063a9ef3da085a
maxdevelopment <gtamadmax@gmailcom>
2015-08-20 15:38:43 +0300

    test eclipse

---
+++ src/local/maxdevelopment/java/MainJava.java
@@ -0,0 +1,72 @@
+package local.maxdevelopment.java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainJava {
+	
+	private static Connection getDBConnection() {
+		
+		String user = "root";			//DB user login
+	    String password = "blackjack";	//DB user password
+	    String url = "jdbc:mysql://localhost:3306/CSVBase";	//DB URL
+	    String driver = "com.mysql.jdbc.Driver";			//DB driver
+	    
+	    Connection dbConnection = null;
+	    
+	    try {
+	        Class.forName(driver);
+	    } catch (ClassNotFoundException e) {
+	        System.out.println(e.getMessage());
+	    }
+	    try {
+	        dbConnection = DriverManager.getConnection(url, user, password);
+	        return dbConnection;
+	    } catch (SQLException e) {
+	        System.out.println(e.getMessage());
+	    }
+	    return dbConnection;
+	}
+	
+	private static void createDbUserTable() throws SQLException {
+		Connection dbConnection = null;
+	    Statement statement = null;
+	    
+	    String createTableSQL = "CREATE TABLE Customer("
+	            + "name VARCHAR(30) NOT NULL, "
+	            + "surname VARCHAR(30) NOT NULL, "
+	            + "login VARCHAR(30) NOT NULL, "
+	            + "email VARCHAR(30) NOT NULL, "
+	            + "phone VARCHAR(30) NOT NULL "
+	            + ")";
+	    
+	    try {
+	    	dbConnection = getDBConnection();
+	        statement = dbConnection.createStatement();
+	        //make SQL query
+	        statement.execute(createTableSQL);
+	        System.out.println("Table \"Customer\" is created!");
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (statement != null) {
+	            statement.close();
+	        }
+	        if (dbConnection != null) {
+	            dbConnection.close();
+	        }
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+	        createDbUserTable();
+	    } catch (SQLException e) {
+	        System.out.println(e.getMessage());
+	    }
+	}
+
+}
